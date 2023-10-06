148afc38f76e456be3176d3b2b1c2c0daf7ada92
Yuxuan Wang <Eric.Wang@gordon.edu>
2023-10-06 10:24:54 -0400

    test setInterval

--- Windows Scrolling function/script.js
+++ Windows Scrolling function/script.js
@@ -1,9 +1,12 @@
-document.getElementById('next').onclick = function(){
+function next() {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').appendChild(lists[0]);
 }
-document.getElementById('prev').onclick = function(){
+
+function pre(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
-  
+    
+  	
+setInterval(next, 3000);
