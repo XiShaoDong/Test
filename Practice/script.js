201ca113bd72caa709986704e27175df0c3ff748
Yuxuan Wang <Eric.Wang@gordon.edu>
2023-11-14 16:59:47 -0500

    Solve an transition execution problem on page load

--- Practice/script.js
+++ Practice/script.js
@@ -1,11 +1,4 @@
-const toggleBtn = document.querySelector('.toggle_btn')
-const toggleBtnIcon = document.querySelector('.toggle_btn i')
-const dropDownMenu = document.querySelector('.dropdown_menu')
-
-toggleBtn.onclick = function() {
-    dropDownMenu.classList.toggle('open')
-    const isOpen = dropDownMenu.classList.contains('open')
-
-    toggleBtnIcon.classList = isOpen?'fa-solid fa-bars' : 'fa-solid fa-bars'
-
-}
+let getBody =  document.querySelector('.transition_none');
+            window.onload = function(){
+                 getBody.className = '';
+            }
